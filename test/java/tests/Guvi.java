abb73db8f09499e15020fb4affd0ca0e993263ab
Arpithaishitha <arpithass139@gmail.com>
2024-06-06 17:12:29 +0530

    adding task 19 file into branch

---
+++ test/java/tests/Guvi.java
@@ -0,0 +1,83 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class Guvi extends mainChorme{
+    @Test
+    public void id() {
+        //load url
+        chromeDriver.get("https://www.guvi.in/register");
+
+        //locate ID
+        WebElement username = chromeDriver.findElement(By.id("name"));
+        WebElement email = chromeDriver.findElement(By.id("email"));
+        WebElement password = chromeDriver.findElement(By.id("password"));
+        WebElement mobile = chromeDriver.findElement(By.id("mobileNumber"));
+        WebElement signup = chromeDriver.findElement(By.id("signup-btn"));
+        username.sendKeys("Ishitha");
+        email.sendKeys("test@123.com");
+        password.sendKeys("Test@1234");
+        mobile.sendKeys("7892623048");
+        signup.click();
+    }
+    @Test
+    public void className() {
+
+        //load url
+        chromeDriver.get("https://www.guvi.in/register");
+
+        //locate class
+        //cannot do all the fields except signup button
+        //because it has same class name
+        WebElement signup =chromeDriver.findElement(By.className("signup-btn"));
+        signup.click();
+
+    }
+    //Tagname cannot do all the fields
+    //because it has same tag name
+    @Test
+    public void css() {
+
+        //load url
+        chromeDriver.get("https://www.guvi.in/register");
+
+        //locate Css selecter
+        WebElement username = chromeDriver.findElement(By.cssSelector("#name"));
+        WebElement email = chromeDriver.findElement(By.cssSelector("#email"));
+        WebElement password = chromeDriver.findElement(By.cssSelector("#password"));
+        WebElement mobile = chromeDriver.findElement(By.cssSelector("#mobileNumber"));
+        WebElement signup = chromeDriver.findElement(By.cssSelector("#signup-btn"));
+        username.sendKeys("Ishitha");
+        email.sendKeys("test@123.com");
+        password.sendKeys("Test@1234");
+        mobile.sendKeys("7892623048");
+        signup.click();
+    }
+    @Test
+    public void xpath() {
+
+        //load url
+        chromeDriver.get("https://www.guvi.in/register");
+
+        //locate Xpath
+        WebElement username = chromeDriver.findElement(By.xpath("//input[@id='name']"));
+        WebElement email = chromeDriver.findElement(By.xpath("//input[@id='email']"));
+        WebElement password = chromeDriver.findElement(By.xpath("//input[@id='password']"));
+        WebElement mobile = chromeDriver.findElement(By.xpath("//input[@id='mobileNumber']"));
+        WebElement signup = chromeDriver.findElement(By.xpath("//a[@id='signup-btn']"));
+        username.sendKeys("Ishitha");
+        email.sendKeys("test@123.com");
+        password.sendKeys("Test@1234");
+        mobile.sendKeys("7892623048");
+        signup.click();
+
+
+
+
+
+    }
+
+
+}
